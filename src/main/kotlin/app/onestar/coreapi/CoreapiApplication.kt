4f16e465281ab09e27a31b874f27f3d5783cb1b1
Truong Le <truong.le@nuvoair.com>
2024-05-27 13:01:16 -0500

    init commit

---
+++ src/main/kotlin/app/onestar/coreapi/CoreapiApplication.kt
@@ -0,0 +1,11 @@
+package app.onestar.coreapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoreapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoreapiApplication>(*args)
+}
---
+++ src/main/kotlin/app/onestar/coreapi/controller/HelloWorldController.kt
@@ -0,0 +1,14 @@
+package app.onestar.coreapi.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/hello")
+    fun helloWorld(): Mono<String> {
+        return Mono.just("Hello World")
+    }
+}
