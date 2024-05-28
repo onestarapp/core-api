e8f7a32feb1d2a59c0e78d261d676a09bf468a5d
Truong Le <truong.le@nuvoair.com>
2024-05-27 21:43:17 -0500

    add authenticate

---
+++ src/main/kotlin/app/onestar/coreapi/config/SecurityConfiguration.kt
@@ -0,0 +1,4 @@
+package app.onestar.coreapi.config
+
+class SecurityConfiguration {
+}
---
+++ src/main/kotlin/app/onestar/coreapi/controller/publicapi/AuthenticationController.kt
@@ -0,0 +1,13 @@
+package app.onestar.coreapi.controller.publicapi
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController("/api")
+class AuthenticationController {
+    @GetMapping("/authenticate")
+    fun authenticate(): Mono<String> {
+        return Mono.just("Authenticated")
+    }
+}
